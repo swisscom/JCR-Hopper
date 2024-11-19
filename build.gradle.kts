9ccd86b3bfb4ce05e5b2f9d133b5902ea5a55363
Raphael Schweikert <Raphael.Schweikert1@swisscom.com>
2024-11-19 11:39:15 +0100

    test(editor): add simple playwright spec and integrate with gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,14 +2,14 @@ import com.cognifide.gradle.aem.bundle.tasks.bundle
 import com.github.gradle.node.npm.task.NpmTask
 
 plugins {
-    java
-    `maven-publish`
-    alias(libs.plugins.gap.bundle)
-    alias(libs.plugins.gap.pkg)
-    alias(libs.plugins.nebula)
-    alias(libs.plugins.quality)
-    alias(libs.plugins.cpd)
-    alias(libs.plugins.node)
+	java
+	`maven-publish`
+	alias(libs.plugins.gap.bundle)
+	alias(libs.plugins.gap.pkg)
+	alias(libs.plugins.nebula)
+	alias(libs.plugins.quality)
+	alias(libs.plugins.cpd)
+	alias(libs.plugins.node)
 }
 
 group = "com.swisscom.aem.tools"
@@ -18,184 +18,201 @@ description = "JCR Hopper: Migrate AEM with Grace"
 defaultTasks("check")
 
 quality {
-    checkstyleVersion = libs.versions.checkstyle.get()
-    pmdVersion = libs.versions.pmd.get()
-    spotbugsVersion = libs.versions.spotbugs.get()
-    spotbugsEffort = "max" // min, less, more or max
-    spotbugsLevel = "low" // low, medium, high
+	checkstyleVersion = libs.versions.checkstyle.get()
+	pmdVersion = libs.versions.pmd.get()
+	spotbugsVersion = libs.versions.spotbugs.get()
+	spotbugsEffort = "max" // min, less, more or max
+	spotbugsLevel = "low" // low, medium, high
 }
 
 afterEvaluate {
-    tasks.named("check") {
-        dependsOn("checkQualityMain")
-    }
+	tasks.named("check") {
+		dependsOn("checkQualityMain")
+	}
 
-    dependencies {
-        add("spotbugsPlugins", libs.findsecbugs)
-        add("spotbugsPlugins", libs.sbContrib)
-        add("pmd", libs.pmd.ant)
-        add("pmd", libs.pmd.java)
+	dependencies {
+		add("spotbugsPlugins", libs.findsecbugs)
+		add("spotbugsPlugins", libs.sbContrib)
+		add("pmd", libs.pmd.ant)
+		add("pmd", libs.pmd.java)
 
-        // Spotbugs runtime annotations
-        add("compileOnly", libs.spotbugs.annotations)
-    }
+		// Spotbugs runtime annotations
+		add("compileOnly", libs.spotbugs.annotations)
+	}
 }
 
 tasks {
-    withType<JavaCompile> {
-        options.encoding = "UTF-8"
-        options.compilerArgs.add("-Xlint:all")
-        options.compilerArgs.add("-Xlint:-processing")
-        options.compilerArgs.add("-Xlint:-serial")
-        options.compilerArgs.add("-Werror")
-    }
+	withType<JavaCompile> {
+		options.encoding = "UTF-8"
+		options.compilerArgs.add("-Xlint:all")
+		options.compilerArgs.add("-Xlint:-processing")
+		options.compilerArgs.add("-Xlint:-serial")
+		options.compilerArgs.add("-Werror")
+	}
 
-    test {
-        useJUnitPlatform()
-    }
+	test {
+		useJUnitPlatform()
+	}
 
-    javadoc {
-        options.encoding = "UTF-8"
-    }
+	javadoc {
+		options.encoding = "UTF-8"
+	}
 }
 
 java {
-    afterEvaluate {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
+	afterEvaluate {
+		sourceCompatibility = JavaVersion.VERSION_1_8
+		targetCompatibility = JavaVersion.VERSION_1_8
+	}
 
-    dependencies {
-        // Lombok
-        annotationProcessor(libs.lombok)
-        compileOnly(libs.lombok)
+	dependencies {
+		// Lombok
+		annotationProcessor(libs.lombok)
+		compileOnly(libs.lombok)
 
-        // AEM
-        compileOnly(libs.servlet)
-        compileOnly(libs.gson)
-        compileOnly(libs.apacheCommons.lang3)
-        compileOnly(libs.osgi.annotation)
-        compileOnly(libs.osgi.cmpn)
-        compileOnly(libs.jcr)
-        compileOnly(libs.geronimo)
-        compileOnly(libs.spiCommons)
-        compileOnly(libs.sling.annotations)
-        compileOnly(libs.sling.api)
-        compileOnly(libs.sling.models)
+		// AEM
+		compileOnly(libs.servlet)
+		compileOnly(libs.gson)
+		compileOnly(libs.apacheCommons.lang3)
+		compileOnly(libs.osgi.annotation)
+		compileOnly(libs.osgi.cmpn)
+		compileOnly(libs.jcr)
+		compileOnly(libs.geronimo)
+		compileOnly(libs.spiCommons)
+		compileOnly(libs.sling.annotations)
+		compileOnly(libs.sling.api)
+		compileOnly(libs.sling.models)
 
-        // Test framework
-        testImplementation(libs.junit)
-        testImplementation(libs.aemMock)
-        testImplementation(libs.oakMock)
-        testImplementation(libs.mockito)
-        testImplementation(libs.aemUberJar)
-        testImplementation(libs.jsonAssert)
-    }
+		// Test framework
+		testImplementation(libs.junit)
+		testImplementation(libs.aemMock)
+		testImplementation(libs.oakMock)
+		testImplementation(libs.mockito)
+		testImplementation(libs.aemUberJar)
+		testImplementation(libs.jsonAssert)
+	}
 }
 
 node {
-    version.set(libs.versions.node.get())
-    npmVersion.set(libs.versions.npm.get())
-    npmInstallCommand.set("ci")
-    download.set(true)
+	version.set(libs.versions.node.get())
+	npmVersion.set(libs.versions.npm.get())
+	npmInstallCommand.set("ci")
+	download.set(true)
 }
 
 aem {
-    bundleEmbed(libs.apacheCommons.jexl, "org.apache.commons.jexl3.*", export = false)
+	bundleEmbed(libs.apacheCommons.jexl, "org.apache.commons.jexl3.*", export = false)
 
-    tasks {
-        val frontendBuild by registering(NpmTask::class) {
-            dependsOn(npmInstall)
-            npmCommand.set(listOf("run", "build"))
+	tasks {
+		val frontendBuild by registering(NpmTask::class) {
+			dependsOn(npmInstall)
+			npmCommand.set(listOf("run", "build"))
 
-            inputs.dir("src/main/frontend")
-            inputs.file("package-lock.json")
-            outputs.dir(layout.buildDirectory.dir("frontend"))
-        }
+			inputs.dir("src/main/frontend")
+			inputs.file("package-lock.json")
+			outputs.dir(layout.buildDirectory.dir("frontend"))
+		}
 
-        val lint by registering(NpmTask::class) {
-            dependsOn(npmInstall)
-            npmCommand.set(listOf("run", "test:lint"))
+		val lint by registering(NpmTask::class) {
+			dependsOn(npmInstall)
+			npmCommand.set(listOf("run", "test:lint"))
 
-            inputs.dir("src/main/frontend")
-            inputs.file("package-lock.json")
-        }
+			inputs.dir("src/main/frontend")
+			inputs.file("package-lock.json")
+		}
 
-        val tsc by registering(NpmTask::class) {
-            dependsOn(npmInstall)
-            npmCommand.set(listOf("run", "test:compile"))
+		val tsc by registering(NpmTask::class) {
+			dependsOn(npmInstall)
+			npmCommand.set(listOf("run", "test:compile"))
 
-            inputs.dir("src/main/frontend")
-            inputs.file("package-lock.json")
-        }
+			inputs.dir("src/main/frontend")
+			inputs.file("package-lock.json")
+		}
 
-        val format by registering(NpmTask::class) {
-            dependsOn(npmInstall)
-            npmCommand.set(listOf("run", "test:format"))
+		val playwrightInstall by registering(NpmTask::class) {
+			dependsOn(npmInstall)
+			npmCommand.set(listOf("run", "install:playwright"))
+			outputs.upToDateWhen { false }
+		}
 
-            inputs.dir("src")
-            inputs.file("package-lock.json")
-            inputs.file(".prettierrc.json")
-        }
+		val playwright by registering(NpmTask::class) {
+			mustRunAfter(playwrightInstall)
+			dependsOn(npmInstall)
+			dependsOn(frontendBuild)
+			npmCommand.set(listOf("run", "test:playwright"))
 
-        val prettierFormat by registering(NpmTask::class) {
-            dependsOn(npmInstall)
-            npmCommand.set(listOf("run", "test:format", "--", "--write"))
-            outputs.upToDateWhen { false }
-        }
+			inputs.dir("src/test/playwright")
+			inputs.file("configs/playwright.config.ts")
+			outputs.dir(project.layout.buildDirectory.dir("reports/$name"))
+		}
 
-        packageValidate {
-            enabled = true
-        }
+		val format by registering(NpmTask::class) {
+			dependsOn(npmInstall)
+			npmCommand.set(listOf("run", "test:format"))
 
-        check {
-            dependsOn(lint, tsc, format, packageValidate)
-        }
+			inputs.dir("src")
+			inputs.file("package-lock.json")
+			inputs.file(".prettierrc.json")
+		}
 
-        val aemContent by registering(Sync::class) {
-            from("$projectDir/src/main/content")
-            from(frontendBuild)
+		val prettierFormat by registering(NpmTask::class) {
+			dependsOn(npmInstall)
+			npmCommand.set(listOf("run", "test:format", "--", "--write"))
+			outputs.upToDateWhen { false }
+		}
 
-            destinationDir = project.layout.buildDirectory.dir(name).get().asFile
-            packageOptions.contentDir.set(destinationDir)
-        }
+		packageValidate {
+			enabled = true
+		}
 
-        assemble {
-            dependsOn(aemContent)
-        }
+		check {
+			dependsOn(lint, tsc, format, packageValidate, playwright)
+		}
 
-        packagePrepare {
-            dependsOn(aemContent)
-        }
+		val aemContent by registering(Sync::class) {
+			from("$projectDir/src/main/content")
+			from(frontendBuild)
 
-        withType<Jar> {
-            bundle {
-                javaPackage.set("${project.group}.jcrhopper")
-            }
-        }
-    }
+			destinationDir = project.layout.buildDirectory.dir(name).get().asFile
+			packageOptions.contentDir.set(destinationDir)
+		}
+
+		assemble {
+			dependsOn(aemContent)
+		}
+
+		packagePrepare {
+			dependsOn(aemContent)
+		}
+
+		withType<Jar> {
+			bundle {
+				javaPackage.set("${project.group}.jcrhopper")
+			}
+		}
+	}
 }
 
 publishing {
-    repositories {
-        val mavenPublishUser: String? by project
-        val mavenPublishPassword: String? by project
-        if (mavenPublishUser != null && mavenPublishPassword != null) {
-            maven {
-                name = "GitHub"
-                url = uri("https://maven.pkg.github.com/swisscom/JCR-Hopper")
-                credentials {
-                    username = mavenPublishUser
-                    password = mavenPublishPassword
-                }
-            }
-        }
-    }
+	repositories {
+		val mavenPublishUser: String? by project
+		val mavenPublishPassword: String? by project
+		if (mavenPublishUser != null && mavenPublishPassword != null) {
+			maven {
+				name = "GitHub"
+				url = uri("https://maven.pkg.github.com/swisscom/JCR-Hopper")
+				credentials {
+					username = mavenPublishUser
+					password = mavenPublishPassword
+				}
+			}
+		}
+	}
 
-    publications {
-        create<MavenPublication>("hopper") {
-            artifact(tasks.packageCompose)
-            from(components["java"])
-        }
-    }
+	publications {
+		create<MavenPublication>("hopper") {
+			artifact(tasks.packageCompose)
+			from(components["java"])
+		}
+	}
 }
